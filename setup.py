31be9c586f67aa1dea3e42aa07b0819749bbc59a
Turvash <singhturvash@rediffmail.com>
2025-09-28 15:17:26 +0530

    Project Structure Ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a complete end-to-end Implemented Capstone Project.',
+    author='Turvash Singh',
+    license='MIT',
+)
